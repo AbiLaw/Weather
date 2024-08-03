b1662c0829ab4a364b8b8cb26d9806cae2ef6868
AbiLaw <91369347+AbiLaw@users.noreply.github.com>
2024-08-03 16:53:39 +0100

    Search functionality with API and axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+
+
+
+function refreshWeather(response){
+    let temperatureElement= document.querySelector (`#temp`)
+    let temp = response.data.temperature.current;
+ let cityElement = document.querySelector(`#city`);
+ cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temp);
+}
+
+
+function searchCity(city){
+let apiKey = "3ae4fbd5aec02f8bf63o3f22ta2b0b5c";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit (event){
     event.preventDefault ();
     let searchInput = document.querySelector ("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+  
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
